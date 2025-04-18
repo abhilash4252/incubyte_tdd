a54c9e64fe09df321c84a2c7906f3036d05395ad
Abhilash Kotte <abhilash4252@gmail.com>
2025-04-18 13:07:18 +0530

    Add failing specs for delimeter new line

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,10 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two comma-separated numbers' do
       expect(StringCalculator.add("1,2")).to eq(3)
     end
+
+    it 'handles newlines as delimiters in addition to commas' do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+        expect(StringCalculator.add("4\n5\n6")).to eq(15)
+    end
   end
 end
