320f6235782d1a2236c505ae6b37e10558024a12
Abhilash Kotte <abhilash4252@gmail.com>
2025-04-18 13:09:36 +0530

    Add tests for custom delimeter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,5 +18,10 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1\n2,3")).to eq(6)
         expect(StringCalculator.add("4\n5\n6")).to eq(15)
     end
+
+    it 'supports a custom single-character delimiter defined in the format //delimiter\\n' do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+        expect(StringCalculator.add("//*\n2*3*4")).to eq(9)
+    end
   end
 end
