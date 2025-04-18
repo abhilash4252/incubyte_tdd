64c6d87f9098f0bd3a649d3bff4863d6625499c3
Abhilash Kotte <abhilash4252@gmail.com>
2025-04-18 13:03:26 +0530

    Add specs for single and comma seperated values

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+  
+    it 'returns the number itself for a single number string' do
+      expect(StringCalculator.add("5")).to eq(5)
+    end
+
+    it 'returns the sum of two comma-separated numbers' do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
   end
 end
