24fe7df30aebf42aac3e5c0316c127e2ae0d9762
Abhilash Kotte <abhilash4252@gmail.com>
2025-04-18 13:10:06 +0530

    Make test cases pass

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,13 @@ class StringCalculator
     def self.add(input)
         return 0 if input.empty?
 
-        numbers = input.split(/[\n,]/).map(&:to_i)
+        if input.start_with?("//")
+          delimiter, input = input.match(%r{//(.)\n(.*)})[1..2]
+          numbers = input.split(/[\n#{Regexp.escape(delimiter)}]/).map(&:to_i)
+        else
+          numbers = input.split(/[\n,]/).map(&:to_i)
+        end
+    
         numbers.sum
     end
 end
